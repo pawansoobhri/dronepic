4ba8f7a261a189db4674a0253ab14b8e675e5542
pawansoobhri <pawanrockz93@gmail.com>
2017-03-15 15:06:16 +0530

    feed

--- feed.php
+++ feed.php
@@ -1,12 +1,13 @@
  <?php
  header('Access-Control-Allow-Origin: *');
- /*header("Content-Type: application/json");*/
+ header("Content-Type: application/json");
  $basicfeed = array();
  $basicfeed["profile"] = array();
  $basicfeed["profile"]["username"] = "eric";
 
- $feed = $json_encode($basicfeed);
+ $feed = json_encode($basicfeed);
  echo($feed);
 
  ?>
+ 
  
