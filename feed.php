92980f20e7900d879b72f260c9520f6082280c27
pawansoobhri <pawanrockz93@gmail.com>
2017-03-15 15:17:09 +0530

    read json

--- feed.php
+++ feed.php
@@ -1,13 +1,8 @@
  <?php
  header('Access-Control-Allow-Origin: *');
  header("Content-Type: application/json");
- $basicfeed = array();
- $basicfeed["profile"] = array();
- $basicfeed["profile"]["username"] = "eric";
-
- $feed = json_encode($basicfeed);
- echo($feed);
-
+ $resJSON = file_get_contents('res.json');
+echo $resJSON;
  ?>
  
  
